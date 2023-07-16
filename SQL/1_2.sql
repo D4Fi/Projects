56f2524aaa520b4d39af6f3d3cc58dc5e617e936
Daniel.Dev <139288494+D4Fi@users.noreply.github.com>
2023-07-16 14:44:40 -0300

    Add files via upload

---
+++ SQL/1_2.sql
@@ -0,0 +1,10 @@
+/**/
+
+/*EXECUTE UM SELECT SIMPLES*/
+select * from Teste;
+
+/*⦁	EXECUTE UM SELECT SIMPLES COM APENAS UMA COLUNA*/
+select x from Teste;
+
+/*⦁	EXECUTE UM SELECT COM MAIS DE UMA COLUNA*/
+select x,y from Teste;
