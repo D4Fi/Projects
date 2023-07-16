58ea1ac1097a98d52609f70207652bfcf58660e2
Daniel.Dev <139288494+D4Fi@users.noreply.github.com>
2023-07-16 15:23:43 -0300

    Delete WEB directory

--- WEB/JS/0.5/script.js
+++
@@ -1,7 +0,0 @@
-
-function mudarCor() {
-	var listColor = ["red",  "blue", "green", "gray"]
-	var color = document.querySelector(".container")
-	color.style.background = listColor[Math.floor(Math.random() * 10)]
-
-}
