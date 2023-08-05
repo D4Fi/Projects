48db09ce38ca56dc4c333decf66b2846fcdaf6d4
Daniel.Dev <139288494+D4Fi@users.noreply.github.com>
2023-08-05 20:25:06 -0300

    Add files via upload

---
+++ DJANGO/0.2/recipes/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from recipes.views import home
+
+urlpatterns = [
+    path('', home),
+]
---
+++ DJANGO/0.2/recipes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'daniel',
+        })
+
+
+
+
+
+
+
