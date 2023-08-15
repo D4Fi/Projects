a7b87f963feb56a503adef1bc3a9ece3a2b1ebd0
Daniel.Dev <139288494+D4Fi@users.noreply.github.com>
2023-08-14 21:01:54 -0300

    Update settings.py

--- DJANGO/0.7/a/settings.py
+++ DJANGO/0.7/a/settings.py
@@ -56,9 +56,5 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
