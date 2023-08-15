42d3f723bd58a62ac7db40b0355c2752a8bf1cf6
Daniel.Dev <139288494+D4Fi@users.noreply.github.com>
2023-08-14 21:02:03 -0300

    Update settings.py

--- DJANGO/0.7/a/settings.py
+++ DJANGO/0.7/a/settings.py
@@ -8,15 +8,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
 
 ROOT_URLCONF = 'a.urls'
 TEMPLATES = [
