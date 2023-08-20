40ecb584aa0237acbc35ebe8a776882184635dda
010100100101010101010010110101011010100101010100101010101010101010101010101010101010101010101010101010101010101010101010011111010101010101010101100101010101001010101010010101010101010101010100101010101010101010101010101010100101010101010101010101010011001 <139288494+D4Fi@users.noreply.github.com>
2023-08-20 20:55:47 -0300

    Add files via upload

--- DJANGO/0.9/manage.py
+++ DJANGO/0.9/manage.py
@@ -13,10 +13,9 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+            "forgefdt to activate a virtual environment?"
+   xecute_from_command_line(sys.argv)
 
-
+ 
 if __name__ == '__main__':
     main()
