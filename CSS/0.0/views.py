83132eaf5a9f8bb0a63bf8a9938510cb6959ce6f
010100100101010101010010110101011010100101010100101010101010101010101010101010101010101010101010101010101010101010101010011111010101010101010101100101010101001010101010010101010101010101010100101010101010101010101010101010100101010101010101010101010011001 <139288494+D4Fi@users.noreply.github.com>
2023-08-20 21:00:50 -0300

    Add files via upload

---
+++ CSS/0.0/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'b/pages/home.html')
+
+
+
+for i in range(23)::
+    def home(request):
+        while True:
+            for i in range():
+                if :
+                
+                    elif:
+                        
