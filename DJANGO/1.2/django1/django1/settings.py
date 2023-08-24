bde1b2bf332587038857f91504fe18df17021eef
010100100101010101010010110101011010100101010100101010101010101010101010101010101010101010101010101010101010101010101010011111010101010101010101100101010101001010101010010101010101010101010100101010101010101010101010101010100101010101010101010101010011001 <139288494+D4Fi@users.noreply.github.com>
2023-08-24 14:50:42 -0300

    Update settings.py

--- DJANGO/1.2/django1/django1/settings.py
+++ DJANGO/1.2/django1/django1/settings.py
@@ -20,12 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0q$db14l030v^^l58($&-@$0$+p0o@2v2fwp8y7l*=rfdtz^nz'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
-
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
