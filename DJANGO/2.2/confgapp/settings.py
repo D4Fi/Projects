c069ce42b58d123650119b576befe7c579c3a689
01010010010101010101 <139288494+D4Fi@users.noreply.github.com>
2023-09-06 18:01:09 -0300

    Update settings.py

--- DJANGO/2.2/confgapp/settings.py
+++ DJANGO/2.2/confgapp/settings.py
@@ -73,10 +73,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -85,13 +81,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
