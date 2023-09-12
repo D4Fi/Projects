4245905a88002d08d6e6db6e3fc95de7581077db
01010010010101010101 <139288494+D4Fi@users.noreply.github.com>
2023-09-12 18:37:59 -0300

    Add files via upload

---
+++ DJANGO/2.3/2.3/core/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from . import models
+from . import forms
+
+
+
+def index(request):
+    return render(request, 'core/pages/index.html')
+
+def contact(request):
+    form = forms.ContactForms(request.method or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            messages.success(request,'Envioooooooooooooo!!!!!')
+            form = forms.ContactForms()
+        else:
+            messages.error(request,'Errooooorrrrrrrrrrrr!!!!!')
+
+    return render(request, 'core/pages/contact.html',
+                  context={
+                      'form': forms.ContactForms(),
+                      }
+                  )
+
+def product(request):
+    return render(request, 'core/pages/product.html')
