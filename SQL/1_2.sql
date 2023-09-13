2689915d9736e7f4e3000bbcba40803b1fddaaa0
01010010010101010101 <139288494+D4Fi@users.noreply.github.com>
2023-09-13 10:53:38 -0300

    Delete SQL directory

--- SQL/1_2.sql
+++
@@ -1,8 +0,0 @@
-/*EXECUTE UM SELECT SIMPLES*/
-select * from Teste;
- 
-/*⦁	EXECUTE UM SELECT SIMPLES COM APENAS UMA COLUNA*/
-select x from Teste;
-
-/*⦁	EXECUTE UM SELECT COM MAIS DE UMA COLUNA*/
-select x,y from Teste;
