fe49b30c34dd25c7a9901ee501cbf8595655cf6a
01010010010101010101 <139288494+D4Fi@users.noreply.github.com>
2023-09-13 11:44:45 -0300

    Delete SQL directory

--- SQL/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class ContactForms(forms.Form):
-    name = forms.CharField(label='Name')
-    email = forms.EmailField(label='E-mail')
-    subject = forms.CharField(label='Subject')
-    message = forms.CharField(label='Message', widget=forms.Textarea())
